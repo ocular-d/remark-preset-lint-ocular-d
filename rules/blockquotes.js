d0a2003d1dffe7088cef103b4f495fdb3d35b382
sven <sven@so36.net>
2019-12-30 14:33:28 +0100

    chore(blockquotes): update links

--- rules/blockquotes.js
+++ rules/blockquotes.js
@@ -1,15 +1,12 @@
 /*
- * Copyright (c) 2018-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2018-present Sven Greb <code@svengreb.de>
- *
- * Project:    Arctic Ice Studio remark-lint Preset
+ * Based on Arctic Ice Studio remark-lint Preset
  * Repository: https://github.com/arcticicestudio/remark-preset-lint-arcticicestudio
  * License:    MIT
  */
 
 /**
  * @file Official remark-lint core rules for blockquote document nodes.
- * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Arctic Ice Studio <development@arcticicestudio.com> and ocular-d
  * @see {@link https://github.com/remarkjs/remark-lint/blob/master/doc/rules.md#list-of-rules remark-lint Core Rules}
  */
 
@@ -18,7 +15,7 @@
  *
  * @since 0.1.0
  * @see {@link https://github.com/remarkjs/remark-lint/tree/master/packages/remark-lint-blockquote-indentation blockquote-indentation}
- * @see {@link https://arcticicestudio.github.io/styleguide-markdown/rules/blockquotes.html#indentation Style Guide: "Blockquotes - Indentation"}
+ * @see {@link https://ocular-d.github.io/styleguide-markdown/blockquotes.html#indentation Style Guide: "Blockquotes - Indentation"}
  */
 const blockquoteIndentation = require("remark-lint-blockquote-indentation");
 /**
@@ -26,7 +23,7 @@ const blockquoteIndentation = require("remark-lint-blockquote-indentation");
  *
  * @since 0.1.0
  * @see {@link https://github.com/remarkjs/remark-lint/tree/master/packages/remark-lint-no-blockquote-without-marker remark-lint-no-blockquote-without-marker}
- * @see {@link https://arcticicestudio.github.io/styleguide-markdown/rules/blockquotes.html#multi-line Style Guide: "Blockquotes - Multi Line"}
+ * @see {@link https://ocular-d.github.io/styleguide-markdown/blockquotes.html#multi-line Style Guide: "Blockquotes - Multi Line"}
  */
 const noBlockquoteWithoutMarker = require("remark-lint-no-blockquote-without-marker");
 
