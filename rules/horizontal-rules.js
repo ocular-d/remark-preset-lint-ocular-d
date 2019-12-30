25899bb6e4a9ae2528b0c3f7294a109070c7a8ec
sven <sven@so36.net>
2019-12-30 14:47:48 +0100

    chore(horizontal-rules): update links

--- rules/horizontal-rules.js
+++ rules/horizontal-rules.js
@@ -1,15 +1,12 @@
 /*
- * Copyright (c) 2018-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2018-present Sven Greb <code@svengreb.de>
- *
- * Project:    Arctic Ice Studio remark-lint Preset
+ * Based on Arctic Ice Studio remark-lint Preset
  * Repository: https://github.com/arcticicestudio/remark-preset-lint-arcticicestudio
  * License:    MIT
  */
 
 /**
  * @file Official remark-lint core rules for horizontal rule document nodes.
- * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Arctic Ice Studio <development@arcticicestudio.com> and ocular-d
  * @see {@link https://github.com/remarkjs/remark-lint/blob/master/doc/rules.md#list-of-rules remark-lint Core Rules}
  */
 
@@ -18,7 +15,7 @@
  *
  * @since 0.1.0
  * @see {@link https://github.com/remarkjs/remark-lint/tree/master/packages/remark-lint-rule-style rule-style}
- * @see {@link https://arcticicestudio.github.io/styleguide-markdown/rules/horizontal-rules.html#marker-character-style Style Guide: "Horizontal Rules - Marker Character Style"}
+ * @see {@link https://ocular-d.github.io/styleguide-markdown/horizontal-rules.html#marker-character-style Style Guide: "Horizontal Rules - Marker Character Style"}
  */
 const ruleStyle = require("remark-lint-rule-style");
 
