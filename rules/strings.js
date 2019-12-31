0707409132146814f496f2fd30871e0b4ddbf51d
ocular-d <sven@ocular-d.tech>
2019-12-31 09:53:25 +0100

    chore(strings): update links

--- rules/strings.js
+++ rules/strings.js
@@ -1,15 +1,12 @@
 /*
- * Copyright (c) 2018-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2018-present Sven Greb <code@svengreb.de>
- *
- * Project:    Arctic Ice Studio remark-lint Preset
+ * Based on Arctic Ice Studio remark-lint Preset
  * Repository: https://github.com/arcticicestudio/remark-preset-lint-arcticicestudio
  * License:    MIT
  */
 
 /**
  * @file Official remark-lint core rules for string document nodes.
- * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Arctic Ice Studio <development@arcticicestudio.com> and ocular-d
  * @see {@link https://github.com/remarkjs/remark-lint/blob/master/doc/rules.md#list-of-rules remark-lint Core Rules}
  */
 
@@ -18,7 +15,7 @@
  *
  * @since 0.1.0
  * @see {@link https://github.com/remarkjs/remark-lint/tree/master/packages/remark-lint-link-title-style link-title-style}
- * @see {@link https://arcticicestudio.github.io/styleguide-markdown/rules/strings.html#quotes Style Guide: "Strings - Quotes"}
+ * @see {@link https://ocular-d.github.io/styleguide-markdown/strings.html#quotes Style Guide: "Strings - Quotes"}
  */
 const linkTitleStyle = require("remark-lint-link-title-style");
 
